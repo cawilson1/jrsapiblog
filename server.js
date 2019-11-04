28e282071ea6be06b10037b3552c8b963d750779
User <user@Users-MacBook-Air.local>
2019-11-04 10:44:36 -0500

    update server

--- server.js
+++ server.js
@@ -6,15 +6,42 @@ const app = express()
 const PORT = 3000
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/todos',{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect('mongodb://localhost:27017/todos', { useNewUrlParser: true, useUnifiedTopology: true })
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const user = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const UserModel = mongoose.model('user', user)
+
+app.post('/user', async (request, response) => {
+    try {
+        console.log("POST USER")
+        var userInstance = new UserModel(request.body)
+        console.log(userInstance)
+        const created = await UserModel.create(userInstance)
+        response.send(created)
+    } catch (error) {
+        response.status(500).send(error)
+    }
+}
+
+)
+
 const start = () => {
-    return app.listen(PORT, ()=>{
+    return app.listen(PORT, () => {
         console.log(`server is running on ${PORT}`)
     })
 }
 
-module.exports = {start}
+module.exports = { start }
